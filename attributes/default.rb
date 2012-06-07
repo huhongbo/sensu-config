a56b5ed32b07c34689aa8cd395aacd5e611c9e18
dn365 <duning3650@gmail.com>
2012-06-07 12:57:38 +0800

    add ruby memory on hpux

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default["conf_json"] = ["hp.json", "aix.json", "handlers.json", "checks.json", "
 
 default["plugin_files"] = ["vmstat-metrics-aix.rb", "check_load.rb", 
   "interface_network.rb", "vmstat-metrics.rb", "disk-metrics.rb", "memory-metrics.rb", 
-  "load.rb", "vmstat-metrics-hpux.rb"]
+  "load.rb", "vmstat-metrics-hpux.rb", "hpux-ruby-memory.rb"]
   
 default["handler_files"] = ["file.rb", "stdout.rb"]
