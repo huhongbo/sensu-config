3440c702686cc74f9e174436aa4aba359d077cb3
dn365 <duning3650@gmail.com>
2012-06-12 11:32:03 +0800

    Upgrade load script

--- files/default/plugins/load.rb
+++ files/default/plugins/load.rb
@@ -2,18 +2,12 @@
 #
 # System VMStat Plugin
 # ===
-#
-# This plugin uses vmstat to collect basic system metrics, produces
-# Graphite formated output.
-#
-# Copyright 2011 Sonian, Inc <chefs@sonian.net>
-#
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
+# code dn365
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
 require 'socket'
+require 'sigar'
 
 class VMStat < Sensu::Plugin::Metric::CLI::Graphite
 
@@ -22,56 +16,17 @@ class VMStat < Sensu::Plugin::Metric::CLI::Graphite
     :long => "--scheme SCHEME",
     :default => "#{Socket.gethostname}.load"
 
-  def convert_integers(values)
-    values.each_with_index do |value, index|
-      begin
-        converted = Integer(value)
-        values[index] = converted
-      rescue ArgumentError
-      end
-    end
-    values
-  end
-
   def run
-    result = convert_integers(`uptime|cut -d',' -f 4-|cut -d':' -f 2-`.lstrip.split.join("").split(","))
+
+    loadavg = Sigar.new.loadavg
     timestamp = Time.now.to_i
+
     metrics = {
       :load => {
-        :onemonute => result[0],
-        :fivemonute => result[1],
-        :fifteenmonute => result[2]
+        :onemonute => loadavg[0],
+        :fivemonute => loadavg[1],
+        :fifteenmonute => loadavg[2]
       }
-=begin
-      :procs => {
-         :waiting => result[0],
-         :uninterruptible => result[1]
-       },
-       :memory => {
-         :swap_used => result[2],
-         :free => result[3],
-         :buffers => result[4],
-         :cache => result[5]
-       },
-       :swap => {
-         :in => result[6],
-         :out => result[7]
-       },
-       :io => {
-         :received => result[8],
-         :sent => result[9]
-       },
-       :system => {
-         :interrupts_per_second => result[10],
-         :context_switches_per_second => result[11]
-       },
-       :cpu => {
-         :user => result[12],
-         :system => result[13],
-         :idle => result[14],
-         :waiting => result[15]
-      }
-=end
     }
     metrics.each do |parent, children|
       children.each do |child, value|
