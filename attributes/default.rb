1dd63e79875c13a3b51a261761fe9dee9e4d58e8
dn365 <duning3650@gmail.com>
2012-06-13 10:58:12 +0800

    add new plugins

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default["conf_json"] = ["hp.json", "aix.json", "handlers.json", "checks.json", "web.json"]
 
-default["plugin_files"] = ["vmstat-metrics-aix.rb", "check_load.rb", 
-  "interface_network.rb", "vmstat-metrics.rb", "disk-metrics.rb", "memory-metrics.rb", 
+default["plugin_files"] = ["vmstat-metrics-aix.rb", "df.rb", 
+  "network.rb", "vmstat-metrics.rb", "disk-metrics.rb", "memory.rb", 
   "load.rb", "vmstat-metrics-hpux.rb", "hpux-ruby-memory.rb"]
   
 default["handler_files"] = ["file.rb", "stdout.rb"]
