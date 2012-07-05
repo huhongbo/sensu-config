4e0de600b4744dd3af945a60c953acb311d08630
dn365 <duning3650@gmail.com>
2012-07-05 10:07:51 +0800

    增加sensu client 进程状态监控，对异常进程重新初始

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
 maintainer       "sensu config"
 maintainer_email "dn@365"
 license          "All rights reserved"
-description      "Installs/Configures sensu-config add aix and hpux add server"
+description      "Installs/Configures sensu-config add aix and hpux add server, add check sensu client process status"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 
 # https://github.com/dn365/chef-base-config
-depends "chef-base-config"
+#depends "chef-base-config"
 
 # https://github.com/dn365/chef-client
-depends "chef-client"
+#depends "chef-client"
 
 
 %w[ ubuntu hpux aix centos redhat ].each do |os|
