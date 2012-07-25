b40a940d0bb7839fe0b0e606212c7afb4394291f
dn365 <duning3650@gmail.com>
2012-07-25 13:06:09 +0800

    v 0.0.9

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dn@365"
 license          "All rights reserved"
 description      "Installs/Configures sensu-config add aix and hpux add server, add check sensu client process status"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.9"
 
 
 # https://github.com/dn365/chef-base-config
